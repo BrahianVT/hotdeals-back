d9b21c61ef6df99b075425b8c190f42528009151
BrahianVT <pumasemj@hotmail.com>
2025-07-15 21:29:35 -0600

    added ceda stuff

--- mongo-init.js
+++ mongo-init.js
@@ -5,27 +5,27 @@ db.createCollection('deals', {capped: false})
 
 db.categories.insertMany([
   {
-    names: {en: 'Computers', tr: 'Bilgisayar'},
+    names: {en: 'Flores y Hortalizas', tr: 'Bilgisayar'},
     parent: '/',
-    category: '/computers',
+    category: '/flores_Hortalizas',
     iconLigature: 'computer_baseline',
     iconFontFamily: 'MaterialIcons',
     isTag: false,
     _class: 'category'
   },
   {
-    names: {en: 'Electronics', tr: 'Elektronik '},
+    names: {en: 'Abarrotes y Víveres', tr: 'Elektronik '},
     parent: '/',
-    category: '/electronics',
+    category: '/abarrotes_Viveres',
     iconLigature: 'devices_baseline',
     iconFontFamily: 'MaterialIcons',
     isTag: false,
     _class: 'category'
   },
   {
-    names: {en: 'Video Cards', tr: 'Ekran Kartı'},
-    parent: '/computers',
-    category: '/computers/video-cards',
+    names: {en: 'Leches', tr: 'Ekran Kartı'},
+    parent: '/abarrotes_Viveres',
+    category: '/abarrotes_Viveres/leches',
     iconLigature: 'memory_baseline',
     iconFontFamily: 'MaterialIcons',
     isTag: false,
@@ -35,68 +35,67 @@ db.categories.insertMany([
 
 db.stores.insertMany([
   {
-    name: 'Amazon',
+    name: 'CEDA',
     logo: 'https://logoeps.com/wp-content/uploads/2011/05/amazon-logo-vector.png',
-    _class: 'store'
-  },
-  {
-    name: 'BestBuy',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Best_Buy_Logo.svg/1280px-Best_Buy_Logo.svg.png',
-    _class: 'store'
-  },
-  {
-    name: 'Walmart',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Walmart_logo.svg/800px-Walmart_logo.svg.png',
     _class: 'store'
   },
 ])
 
 db.categories.insertMany([
   {
-    names: {en: 'Sale', tr: 'İndirim'},
+    names: {en: 'Frutas y Legumbres', tr: 'İndirim'},
     parent: '/',
-    category: '/sale',
+    category: '/frutas_Legumbres',
     iconLigature: 'local_offer_baseline',
     iconFontFamily: 'MaterialIcons',
-    isTag: true,
+    isTag: false,
     _class: 'category'
   },
   {
-    names: {en: 'Discount', tr: 'İndirim'},
+    names: {en: 'Pernocta', tr: 'İndirim'},
     parent: '/',
-    category: '/discount',
+    category: '/pernocta',
     iconLigature: 'money_off_baseline',
     iconFontFamily: 'MaterialIcons',
-    isTag: true,
+    isTag: false,
     _class: 'category'
   },
   {
-    names: {en: 'Free Shipping', tr: 'Ücretsiz Kargo'},
+    names: {en: 'Subasta y Productores', tr: 'Ücretsiz Kargo'},
     parent: '/',
-    category: '/free-shipping',
+    category: '/subasta',
     iconLigature: 'local_shipping_baseline',
     iconFontFamily: 'MaterialIcons',
-    isTag: true,
+    isTag: false,
     _class: 'category'
   },
   {
-    names: {en: 'Limited Time', tr: 'Sınırlı Süre'},
+    names: {en: 'Envases Vacios', tr: 'Sınırlı Süre'},
     parent: '/',
-    category: '/limited-time',
+    category: '/envases_Vacios',
     iconLigature: 'schedule_baseline',
     iconFontFamily: 'MaterialIcons',
-    isTag: true,
+    isTag: false,
     _class: 'category'
   },
   {
-    names: {en: 'Clearance', tr: 'Stok Temizleme'},
+    names: {en: 'Aves y Cárnicos', tr: 'Stok Temizleme'},
     parent: '/',
-    category: '/clearance',
+    category: '/carnicos',
     iconLigature: 'delete_sweep_baseline',
     iconFontFamily: 'MaterialIcons',
-    isTag: true,
+    isTag: false,
     _class: 'category'
-  }
+  },
+  {
+      names: {en: 'Viga', tr: 'Stok Temizleme'},
+      parent: '/',
+      category: '/viga',
+      iconLigature: 'delete_sweep_baseline',
+      iconFontFamily: 'MaterialIcons',
+      isTag: false,
+      _class: 'category'
+    }
 ])
 
 
@@ -111,9 +110,7 @@ db.users.insertOne({
 
 
 // Get references to inserted documents
-var amazonStore = db.stores.findOne({name: 'Amazon'})
-var bestBuyStore = db.stores.findOne({name: 'BestBuy'})
-var walmartStore = db.stores.findOne({name: 'Walmart'})
+var ceda = db.stores.findOne({name: 'CEDA'})
 
 
 var user = db.users.findOne()
@@ -122,13 +119,13 @@ var user = db.users.findOne()
 db.deals.insertMany([
   {
     postedBy: user._id,
-    store: amazonStore._id,
+    store: ceda._id,
     dealScore: 15,
     upvoters: [],
     downvoters: [],
-    category: '/computers/video-cards',
-    title: 'NVIDIA GeForce RTX 3080 Graphics Card - Limited Stock Available',
-    description: 'The GeForce RTX 3080 delivers the ultra performance that gamers crave, powered by Ampere—NVIDIA\'s 2nd gen RTX architecture. It\'s built with enhanced RT Cores and Tensor Cores, new streaming multiprocessors, and superfast G6X memory for an amazing gaming experience.',
+    category: '/flores_Hortalizas',
+    title: 'Flores rojas por kilo',
+    description: 'Flores rojas por kilo, en ceda',
     originalPrice: 699.99,
     price: 649.99,
     coverPhoto: 'https://m.media-amazon.com/images/I/81qV-i5gBwL._AC_SL1500_.jpg',
@@ -138,58 +135,165 @@ db.deals.insertMany([
       'https://m.media-amazon.com/images/I/81qV-i5gBwL._AC_SL1500_.jpg',
       'https://m.media-amazon.com/images/I/81wkDEMzj+L._AC_SL1500_.jpg'
     ],
-    views: 120,
-    tags: ['/sale', '/limited-time'],
+    views: 10,
+    tags: ['/flores'],
     createdAt: new Date(),
-    updatedAt: new Date(),
     _class: 'deal'
   },
   {
     postedBy: user._id,
-    store: bestBuyStore._id,
+    store: ceda._id,
     dealScore: 8,
     upvoters: [],
     downvoters: [],
-    category: '/electronics',
-    title: 'Sony WH-1000XM4 Wireless Noise-Cancelling Headphones',
-    description: 'Industry-leading noise cancellation technology means you hear every word, note, and tune with incredible clarity, no matter your environment. These headphones feature additional microphones that assist in isolating sound for a more precise listening experience.',
+    category: '/abarrotes_Viveres',
+    title: 'Oferta en sector de abarrotes',
+    description: 'Oferta en sector de abarrotes',
     originalPrice: 349.99,
     price: 278.00,
-    coverPhoto: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6408/6408356_sd.jpg',
+    coverPhoto: 'https://placehold.co/600x400',
     dealUrl: 'https://www.bestbuy.com/site/sony-wh-1000xm4-wireless-noise-cancelling-over-the-ear-headphones-black/6408356.p',
     status: 'ACTIVE',
     photos: [
-      'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6408/6408356_sd.jpg',
-      'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6408/6408356cv11d.jpg'
+      'https://placehold.co/600x400',
+      'https://placehold.co/600x400'
     ],
     views: 85,
-    tags: ['/discount', '/free-shipping'],
+    createdAt: new Date(),
+    _class: 'deal'
+  },
+  {
+    postedBy: user._id,
+    store: ceda._id,
+    dealScore: 23,
+    upvoters: [],
+    downvoters: [],
+    category: '/frutas_Legumbres',
+    title: 'Manzana Verde',
+    description: 'Manzanas verdes baratas',
+    originalPrice: 54.99,
+    price: 50,
+    coverPhoto: '',
+    dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
+    status: 'ACTIVE',
+    photos: [
+      'https://placehold.co/400',
+      'https://placehold.co/400'
+    ],
+    views: 210,
+    tags: ['/manzana'],
     createdAt: new Date(),
     updatedAt: new Date(),
     _class: 'deal'
   },
   {
-    postedBy: user._id,
-    store: walmartStore._id,
-    dealScore: 23,
-    upvoters: [],
-    downvoters: [],
-    category: '/computers',
-    title: 'HP 24mh FHD Monitor with 23.8-Inch IPS Display (1080p)',
-    description: 'OUTSTANDING VISUALS – This FHD display with IPS technology gives you brilliant visuals and unforgettable quality; with a maximum resolution of 1920 x 1080 at 75 Hz, you\'ll experience the image accuracy and wide-viewing spectrums of premium tablets and mobile devices.',
-    originalPrice: 249.99,
-    price: 226.99,
-    coverPhoto: 'https://i5.walmartimages.com/asr/d2a52481-5f27-4d9f-9838-31f346c4e2af.4b5c2f091d5da2853e70a6c9facac8d3.jpeg',
-    dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
-    status: 'ACTIVE',
-    photos: [
-      'https://i5.walmartimages.com/asr/d2a52481-5f27-4d9f-9838-31f346c4e2af.4b5c2f091d5da2853e70a6c9facac8d3.jpeg',
-      'https://i5.walmartimages.com/asr/bb164cdf-f281-4e52-a481-511de4a82ae0.b4b9fa8a32134f56a429231d426d7cbe.jpeg'
-    ],
-    views: 210,
-    tags: ['/clearance', '/discount'],
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    _class: 'deal'
-  }
+      postedBy: user._id,
+      store: ceda._id,
+      dealScore: 100,
+      upvoters: [],
+      downvoters: [],
+      category: '/pernocta',
+      title: 'pernocta oferta pro',
+      description: 'Pernocta',
+      originalPrice: 54.99,
+      price: 50,
+      coverPhoto: '',
+      dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
+      status: 'ACTIVE',
+      photos: [
+        'https://placehold.co/400',
+        'https://placehold.co/400'
+      ],
+      views: 210,
+      createdAt: new Date(),
+      _class: 'deal'
+    },
+  {
+          postedBy: user._id,
+          store: ceda._id,
+          dealScore: 100,
+          upvoters: [],
+          downvoters: [],
+          category: '/subasta',
+          title: 'subasta oferta pro',
+          description: 'subasta',
+          originalPrice: 24.99,
+          price: 20,
+          coverPhoto: 'https://placehold.co/400',
+          dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
+          status: 'ACTIVE',
+          photos: [
+            'https://placehold.co/400',
+            'https://placehold.co/400'
+          ],
+          views: 0,
+          createdAt: new Date(),
+          _class: 'deal'
+        },
+  {
+                  postedBy: user._id,
+                  store: ceda._id,
+                  dealScore: 100,
+                  upvoters: [],
+                  downvoters: [],
+                  category: '/envases_Vacios',
+                  title: 'envases vacios',
+                  description: 'envases',
+                  originalPrice: 24.99,
+                  price: 20,
+                  coverPhoto: 'https://placehold.co/400',
+                  dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
+                  status: 'ACTIVE',
+                  photos: [
+                    'https://placehold.co/400',
+                    'https://placehold.co/400'
+                  ],
+                  views: 0,
+                  createdAt: new Date(),
+                  _class: 'deal'
+                },
+  {
+                                  postedBy: user._id,
+                                  store: ceda._id,
+                                  dealScore: 100,
+                                  upvoters: [],
+                                  downvoters: [],
+                                  category: '/carnicos',
+                                  title: 'kilo de pechuga',
+                                  description: 'pechuga',
+                                  originalPrice: 124.99,
+                                  price: 120,
+                                  coverPhoto: 'https://placehold.co/400',
+                                  dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
+                                  status: 'ACTIVE',
+                                  photos: [
+                                    'https://placehold.co/400',
+                                    'https://placehold.co/400'
+                                  ],
+                                  views: 0,
+                                  createdAt: new Date(),
+                                  _class: 'deal'
+                                },
+  {
+                                                                  postedBy: user._id,
+                                                                  store: ceda._id,
+                                                                  dealScore: 100,
+                                                                  upvoters: [],
+                                                                  downvoters: [],
+                                                                  category: ' /viga',
+                                                                  title: 'pescado kilo',
+                                                                  description: 'pescado kilo',
+                                                                  originalPrice: 124.99,
+                                                                  price: 120,
+                                                                  coverPhoto: 'https://placehold.co/400',
+                                                                  dealUrl: 'https://www.walmart.com/ip/HP-24mh-FHD-Monitor-with-23-8-Inch-IPS-Display-1080p/715520698',
+                                                                  status: 'ACTIVE',
+                                                                  photos: [
+                                                                    'https://placehold.co/400',
+                                                                    'https://placehold.co/400'
+                                                                  ],
+                                                                  views: 0,
+                                                                  createdAt: new Date(),
+                                                                  _class: 'deal'
+                                                                }
 ])
