fce138a5c93e9ddcfb050770857dfc64da983de4
BrahianVT <pumasemj@hotmail.com>
2026-03-19 21:17:25 -0600

    removed is super for users-by-role

--- src/main/java/com/halildurmus/hotdeals/security/role/RoleController.java
+++ src/main/java/com/halildurmus/hotdeals/security/role/RoleController.java
@@ -24,132 +24,91 @@ import java.util.List;
 @RequestMapping("/roles")
 public class RoleController {
 
-    @Autowired private SecurityService securityService;
+  @Autowired
+  private SecurityService securityService;
 
-
-    @Autowired private RoleService service;
+  @Autowired
+  private RoleService service;
 
   @PutMapping
   @IsSuper
   @Operation(summary = "Adds a role to a user in the Firebase")
   @ApiResponses({
-    @ApiResponse(
-        responseCode = "200",
-        description = "The role successfully added",
-        content = @Content),
-    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+      @ApiResponse(responseCode = "200", description = "The role successfully added", content = @Content),
+      @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+      @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
   })
   public void addRole(
-      @Parameter(
-              description = "String representation of the Firebase User ID",
-              example = "ndj2KkbGwIUbfIUH2BT6700AQ832")
-          @RequestParam
-          @NotBlank
-          String uid,
+      @Parameter(description = "String representation of the Firebase User ID", example = "ndj2KkbGwIUbfIUH2BT6700AQ832") @RequestParam @NotBlank String uid,
       @Parameter(description = "User role") @RequestParam Role role) {
     service.add(uid, role);
   }
 
-    @GetMapping("/viewAll")
-    @IsSuper
-    @Operation(summary = "Check all the roles for all users in firebase")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Successful operation",
-                    content =
-                    @Content(
-                            mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class)))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
-    })
-    public List<RoleServiceImpl.UserWithRoles> viewAllRoles() throws FirebaseAuthException {
-        return service.viewAllRoles();
-    }
+  @GetMapping("/viewAll")
+  @IsSuper
+  @Operation(summary = "Check all the roles for all users in firebase")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class)))),
+      @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+      @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+  })
+  public List<RoleServiceImpl.UserWithRoles> viewAllRoles() throws FirebaseAuthException {
+    return service.viewAllRoles();
+  }
 
-    @IsSuper
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Successful operation",
-                    content =
-                    @Content(
-                            mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class)))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
-    })
-    @GetMapping("/{uid}")
-    @Operation(summary = "Check all the roles for single user firebase")
-public RoleServiceImpl.UserWithRoles getUserRoles(@PathVariable String uid) throws FirebaseAuthException {
+  @IsSuper
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class)))),
+      @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+      @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+  })
+  @GetMapping("/{uid}")
+  @Operation(summary = "Check all the roles for single user firebase")
+  public RoleServiceImpl.UserWithRoles getUserRoles(@PathVariable String uid) throws FirebaseAuthException {
     return service.viewAllRolesUser(uid);
-}
+  }
 
   @DeleteMapping
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @IsSuper
   @Operation(summary = "Deletes a role from a user in the Firebase")
   @ApiResponses({
-    @ApiResponse(
-        responseCode = "204",
-        description = "The role successfully deleted",
-        content = @Content),
-    @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-    @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+      @ApiResponse(responseCode = "204", description = "The role successfully deleted", content = @Content),
+      @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+      @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
   })
   public void deleteRole(
-      @Parameter(
-              description = "String representation of the Firebase User ID",
-              example = "ndj2KkbGwIUbfIUH2BT6700AQ832")
-          @RequestParam
-          @NotBlank
-          String uid,
+      @Parameter(description = "String representation of the Firebase User ID", example = "ndj2KkbGwIUbfIUH2BT6700AQ832") @RequestParam @NotBlank String uid,
       @Parameter(description = "User role") @RequestParam Role role) {
     service.delete(uid, role);
   }
 
-    @GetMapping("/me")
-    @Operation(
-            summary = "Returns the authenticated user's roles",
-            security = @SecurityRequirement(name = "bearerAuth"))
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Successful operation",
-                    content =
-                    @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
-    })
-    public RoleServiceImpl.UserWithRoles getAuthenticatedUserRoles() throws FirebaseAuthException {
-        User user = securityService.getUser();
-        return service.viewAllRolesUser(user.getUid());
-    }
+  @GetMapping("/me")
+  @Operation(summary = "Returns the authenticated user's roles", security = @SecurityRequirement(name = "bearerAuth"))
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class))),
+      @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content)
+  })
+  public RoleServiceImpl.UserWithRoles getAuthenticatedUserRoles() throws FirebaseAuthException {
+    User user = securityService.getUser();
+    return service.viewAllRolesUser(user.getUid());
+  }
 
-    @GetMapping("/users-by-role/{role}")
-    @IsSuper
-    @Operation(summary = "Get all users with a specific role")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Successful operation",
-                    content = @Content(
-                            mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class)))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
-            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
-    })
-    public List<RoleServiceImpl.UserWithRoles> getUsersByRole(
-            @Parameter(description = "User role to filter by", example = "ROLE_MODERATOR")
-            @PathVariable Role role) throws FirebaseAuthException {
-        return service.getUsersByRole(role);
-    }
+  @GetMapping("/users-by-role/{role}")
+  @Operation(summary = "Get all users with a specific role")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = RoleServiceImpl.UserWithRoles.class)))),
+      @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+      @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+      @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)
+  })
+  public List<RoleServiceImpl.UserWithRoles> getUsersByRole(
+      @Parameter(description = "User role to filter by", example = "ROLE_MODERATOR") @PathVariable Role role)
+      throws FirebaseAuthException {
+    return service.getUsersByRole(role);
+  }
 }
